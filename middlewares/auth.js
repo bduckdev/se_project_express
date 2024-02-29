65533ab53f12bb5ce6d16a1ed1be12ddeccea86e
Brennan Duck <brennantduck@gmail.com>
2024-02-29 16:47:47 -0500

    fixed config

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = require("../utils/config");
-const { handleErr } = require("../utils/errors");
 
 function auth(req, res, next) {
   const authorization = req.headers.Authorization;
