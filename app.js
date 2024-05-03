86741bdf79336fc2f570f90a328e1499c78c44db
Brennan Duck <brennantduck@gmail.com>
2024-05-03 15:58:30 -0400

    fixed a thing

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { PORT = 3001 } = process.env;
 app.use(cors());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Successfully connected to DB");
   })
